c113a2ea2d33cfe524eae74c60551fae9f9af2c2
ayse <ayse.bat@cern.ch>
2016-03-06 08:20:52 -0600

    QGTag add

---
+++ githup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd config
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_1.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_2.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_3.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_4.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_5.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_6.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_7.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_8.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_MC_9.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_1.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_2.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_3.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_4.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_5.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_6.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_7.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_8.txt
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/cutFile_mainDijetSelection_9.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/300.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/470.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/600.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/800.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/1000.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/1400.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/1800.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/2400.txt
+wget wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/3200.txt
+cd ../src
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_Data_B.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_Data_S.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_Data_bs.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_Data_N.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_QCD_B.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_QCD_S.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_QCD_bs.C
+wget https://raw.githubusercontent.com/aysbt/DijetRootTreeAnalyzerWithQ-GTag/master/analysisClass_mainDijetSelection_QCD_N.C
+exit 0
