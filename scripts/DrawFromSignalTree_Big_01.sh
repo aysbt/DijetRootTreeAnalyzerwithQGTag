0bb4b32e42adfa807c79d640ee9d8eeaa46a7c94
ayse <ayse.bat@cern.ch>
2016-03-17 13:22:01 -0500

    python file add

---
+++ scripts/DrawFromSignalTree_Big_01.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo"QQ"
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M500/ --inputList list_signal/qq_50ns_M500.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M1000/ --inputList list_signal/qq_50ns_M1000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M2000/ --inputList list_signal/qq_50ns_M2000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M3000/ --inputList list_signal/qq_50ns_M3000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M4000/ --inputList list_signal/qq_50ns_M4000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M5000/ --inputList list_signal/qq_50ns_M5000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M6000/ --inputList list_signal/qq_50ns_M6000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M7000/ --inputList list_signal/qq_50ns_M7000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M8000/ --inputList list_signal/qq_50ns_M8000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QQ/M9000/ --inputList list_signal/qq_50ns_M9000.txt --qgcut 0.1
+echo"QG"
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M500/ --inputList list_signal/qg_50ns_M500.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M1000/ --inputList list_signal/qg_50ns_M1000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M2000/ --inputList list_signal/qg_50ns_M2000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M3000/ --inputList list_signal/qg_50ns_M3000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M4000/ --inputList list_signal/qg_50ns_M4000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M5000/ --inputList list_signal/qg_50ns_M5000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M6000/ --inputList list_signal/qg_50ns_M6000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M7000/ --inputList list_signal/qg_50ns_M7000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M8000/ --inputList list_signal/qg_50ns_M8000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/QG/M9000/ --inputList list_signal/qg_50ns_M9000.txt --qgcut 0.1
+echo"GG"
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M500/ --inputList list_signal/gg_50ns_M500.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M1000/ --inputList list_signal/gg_50ns_M1000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M2000/ --inputList list_signal/gg_50ns_M2000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M3000/ --inputList list_signal/gg_50ns_M3000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M4000/ --inputList list_signal/gg_50ns_M4000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M5000/ --inputList list_signal/gg_50ns_M5000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M6000/ --inputList list_signal/gg_50ns_M6000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M7000/ --inputList list_signal/gg_50ns_M7000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M8000/ --inputList list_signal/gg_50ns_M8000.txt --qgcut 0.1
+python DrawFromTree_signal_Big.py --var mjj --xmin 0 --xmax 14000 --bin 14000 --xtitle "m(jj)" --logy --outputDir ../Signal/GG/M9000/ --inputList list_signal/gg_50ns_M9000.txt --qgcut 0.1
